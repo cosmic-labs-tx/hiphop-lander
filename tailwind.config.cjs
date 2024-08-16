2c274a27df04b6d13ba7ad3a50baab9094190419
Paul Morris <paulh.morris@gmail.com>
2024-08-16 14:47:43 -0500

    Some dx, hero, navbar, features, more features

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,20 @@
-/** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      borderRadius: {
+        lg: "0.625rem",
+      },
+      colors: {
+        brand: {
+          yellow: "#FBD300",
+          blue: "#009CCD",
+        },
+      },
       fontFamily: {
-        sans: [
-          "Bricolage Grotesque Variable",
-          "Inter Variable",
-          "Inter",
-          ...defaultTheme.fontFamily.sans,
-        ],
+        sans: ["Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
       },
     },
   },
