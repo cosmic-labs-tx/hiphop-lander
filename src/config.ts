f0b3eb1fd9b72cbbb20356bcfb2bd56cb72777dc
Paul Morris <paulh.morris@gmail.com>
2025-08-26 21:13:35 -0500

    Update loginUrl

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const Config = {
   ctaUrl: "https://hiphopdriving.plumblearning.com/preview",
-  loginUrl: "https://accounts.plumblearning.com/login",
+  loginUrl: "https://accounts.plumblearning.com/sign-in?redirect_url=https://hiphopdriving.plumblearning.com/preview",
 };
