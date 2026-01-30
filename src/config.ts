93dfa0e2fd287a9157819b52989f046420336cbe
Paul Morris <paulh.morris@gmail.com>
2026-01-29 18:55:21 -0600

    Update urls

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,8 @@
 export const Config = {
-  ctaUrl: "https://hiphopdriving.plumblearning.com/preview",
-  loginUrl: "https://accounts.plumblearning.com/sign-in?redirect_url=https://hiphopdriving.plumblearning.com/preview",
+  ctaUrl: encodeURI(
+    "https://hiphopdriving.plumblearning.com/sign-up?redirect_url=https://hiphopdriving.plumblearning.com/preview"
+  ),
+  loginUrl: encodeURI(
+    "https://hiphopdriving.plumblearning.com/sign-in?redirect_url=https://hiphopdriving.plumblearning.com/preview"
+  ),
 };
